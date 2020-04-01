d7e8f8f0944f804bd71eefb9b11d72bd028f11bc
Corentin <corentin.barban@sfr.fr>
2020-04-01 10:08:46 +0200

    Creation du projet

---
+++ src/main/java/com/barban/corentin/miage/m2/miagesousleau/gestionmembre/GestionmembreApplication.java
@@ -0,0 +1,13 @@
+package com.barban.corentin.miage.m2.miagesousleau.gestionmembre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionmembreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionmembreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barban/corentin/miage/m2/miagesousleau/gestionmembre/GestionmembreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barban.corentin.miage.m2.miagesousleau.gestionmembre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionmembreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
